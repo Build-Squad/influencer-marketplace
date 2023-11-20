929e94a4a0949e5cfcc4c3aee13499ab45350e99
varsha1305nav <87652813+varsha1305nav@users.noreply.github.com>
2023-11-20 11:47:09 +0530

    update

--- src/api/marketplace/accounts/models.py
+++ src/api/marketplace/accounts/models.py
@@ -2,19 +2,15 @@
 from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import SET_NULL
+from core.models import Currency
+from core.models import Country
 import uuid
-
-# class Role(models.Model):
-#     id = models.UUIDField(primary_key=True, verbose_name='Role ID', default=uuid.uuid4, editable=False)
-#     name = models.CharField(max_length=50, unique=True)
-
-#     class Meta:
-#         db_table = "role"
 
 class TwitterAccount(models.Model):
     id = models.UUIDField(primary_key=True, verbose_name='Twitter Account ID', default=uuid.uuid4, editable=False)
     twitter_id = models.CharField(max_length=255, blank=True, null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         db_table = "twitter_account"
@@ -59,12 +55,27 @@ class User(AbstractUser):
     last_login = models.DateTimeField(auto_now=True, blank=True)
     otp = models.CharField(max_length=25, blank=True, null=True)
     otp_expiration = models.DateTimeField(blank=True, null=True)
-    twitter_account_id = models.ForeignKey(TwitterAccount, related_name='user_twitter_account_id', on_delete=models.DO_NOTHING,
+    twitter_account = models.ForeignKey(TwitterAccount, related_name='user_twitter_account_id', on_delete=models.DO_NOTHING,
                                          null=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email']
     ordering = ('email',)
 
     class Meta:
         db_table = "user"
+
+class BankAccount(models.Model):
+    
+    id = models.UUIDField(primary_key=True, verbose_name='Account Category ID', default=uuid.uuid4, editable=False)
+    influencer = models.ForeignKey(User, related_name='bank_acc_influencer_id', on_delete=SET_NULL, null=True)
+    account_holder_name = models.CharField(max_length=150, blank=True, null=True)
+    account_number = models.CharField(max_length=50, blank=True, null=True)
+    bank_name = models.CharField(max_length=100, blank=True, null=True)
+    branch_name = models.CharField(max_length=100, blank=True, null=True)
+    account_type = models.CharField(max_length=100, blank=True, null=True)
+    country = models.ForeignKey(Country, related_name='bank_account_country_id', on_delete=SET_NULL, null=True)
+    currency = models.ForeignKey(Currency, related_name='bank_account_currency_id', on_delete=SET_NULL, null=True)
+    is_verified = models.BooleanField(default=True, blank=True, null=True)
+
+    class Meta:
+        db_table = "bank_account"          
--- src/api/marketplace/packages/models.py
+++ src/api/marketplace/packages/models.py
@@ -1,16 +1,60 @@
+from unicodedata import name
 from django.db import models
 import uuid
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db.models import SET_NULL
 from django.conf import settings
+from core.models import Currency
+from core.models import Country
 
 class Package(models.Model):
     id = models.UUIDField(primary_key=True, verbose_name='Package', default=uuid.uuid4, editable=False)
-    influencer_id = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='created_by_user', on_delete=SET_NULL, null=True)
+    influencer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='created_by_user', on_delete=SET_NULL, null=True)
     name = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    # price = 
-    # currency_id = 
-    # status =
-    # publish_date = 
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    currency = models.ForeignKey(Currency, related_name='package_currency_id', on_delete=SET_NULL, null=True)
+    status = models.CharField(max_length=100, blank=True, null=True)
+    publish_date = models.DateTimeField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "package" 
+
+class ServiceMaster(models.Model):
+
+    TYPE_CHOICES = (
+        ('standard', 'standard'),
+        ('custom', 'custom')
+    )
+
+    id = models.UUIDField(primary_key=True, verbose_name='ServiceMaster', default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    limit = models.CharField(max_length=100, blank=True, null=True)
+    type = models.CharField(choices=TYPE_CHOICES, max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "service_master" 
+
+class Service(models.Model):
+    id = models.UUIDField(primary_key=True, verbose_name='Service', default=uuid.uuid4, editable=False)
+    service_master = models.ForeignKey(ServiceMaster, related_name='service_master_id', on_delete=SET_NULL, null=True)
+    package = models.ForeignKey(Package, related_name='service_package_id', on_delete=SET_NULL, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    currency = models.ForeignKey(Currency, related_name='service_currency_id', on_delete=SET_NULL, null=True)
+    status = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        db_table = "service"   
+
+      
+
+
+
+
+
+
