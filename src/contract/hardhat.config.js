20ae5fa6dc76085f29b4a39bde72228d2df6e944
Mudit Mahajan <mudit.m@navtech.io>
2023-11-21 12:03:06 +0530

    Deploy to testnet

--- src/contract/hardhat.config.js
+++ src/contract/hardhat.config.js
@@ -4,4 +4,10 @@ require('@nomiclabs/hardhat-ethers');
 
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
