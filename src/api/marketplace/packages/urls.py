62903ba822677d13812faa1b6c3a5271805c3bc6
Mudit Mahajan <mudit.m@navtech.io>
2023-11-27 14:07:24 +0530

    Use class based API view

--- src/api/marketplace/packages/urls.py
+++ src/api/marketplace/packages/urls.py
@@ -3,20 +3,30 @@
 # Path: src/api/marketplace/packages/urls.py
 #
 from django.urls import path
-from .views import getAllServiceMaster, createServiceMaster, updateServiceMaster, deleteServiceMaster, getAllService, createService, updateService, deleteService, getAllPackage, createPackage, updatePackage, deletePackage
+from .views import ServiceMasterList, ServiceMasterDetail, ServiceList, ServiceDetail, PackageList, PackageDetail
 
 urlpatterns = [
-    path('', getAllPackage),
-    path('create/', createPackage),
-    path('update/<str:pk>/', updatePackage),
-    path('delete/<str:pk>/', deletePackage),
-    path('servicemaster/', getAllServiceMaster),
-    path('servicemaster/create/', createServiceMaster),
-    path('servicemaster/update/<str:pk>/', updateServiceMaster),
-    path('servicemaster/delete/<str:pk>/', deleteServiceMaster),
-    path('service/', getAllService),
-    path('service/create/', createService),
-    path('service/update/<str:pk>/', updateService),
-    path('service/delete/<str:pk>/', deleteService),
+
+
+    path('servicemaster/', ServiceMasterList.as_view()),
+    path('servicemaster/create/', ServiceMasterList.as_view()),
+    path('servicemaster/update/<str:pk>/', ServiceMasterDetail.as_view()),
+    path('servicemaster/delete/<str:pk>/', ServiceMasterDetail.as_view()),
+    path('servicemaster/<str:pk>/', ServiceMasterDetail.as_view()),
+
+
+    path('service/', ServiceList.as_view()),
+    path('service/create/', ServiceList.as_view()),
+    path('service/update/<str:pk>/', ServiceDetail.as_view()),
+    path('service/delete/<str:pk>/', ServiceDetail.as_view()),
+    path('service/<str:pk>/', ServiceDetail.as_view()),
+
+
+    path('', PackageList.as_view()),
+    path('create/', PackageList.as_view()),
+    path('update/<str:pk>/', PackageDetail.as_view()),
+    path('delete/<str:pk>/', PackageDetail.as_view()),
+    path('<str:pk>/', PackageDetail.as_view()),
+
 ]
 
--- src/api/marketplace/packages/views.py
+++ src/api/marketplace/packages/views.py
@@ -1,322 +1,253 @@
-from django.shortcuts import render
-from marketplace.helpers import Pagination
+from marketplace.services import Pagination, handleServerException, handleBadRequest
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import status
+from django.http import Http404
+from rest_framework.views import APIView
 from .models import Package, ServiceMaster, Service
 from core.models import Currency
 from accounts.models import User
 from .serializers import ServiceMasterSerializer, CreateServiceMasterSerializer, ServicesSerializer, CreateServicesSerializer, PackageSerializer, CreatePackageSerializer
 
 # Service Master
-@api_view(['GET'])
-def getAllServiceMaster(request):
-    try:
-      serviceMaster = ServiceMaster.objects.filter(deleted_at=None)
-      pagination = Pagination(serviceMaster, request)
-      serializer = ServiceMasterSerializer(pagination.getData(), many=True)
-      return Response({
-          'code': 200,
-          'success': True,
-          'data': serializer.data,
-          'message': 'All ServiceMaster retrieved successfully',
-          'pagination': pagination.getPageInfo()
-      })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
+class ServiceMasterList(APIView):
+    def get(self, request):
+        try:
+          serviceMaster = ServiceMaster.objects.filter(deleted_at=None)
+          pagination = Pagination(serviceMaster, request)
+          serializer = ServiceMasterSerializer(pagination.getData(), many=True)
+          return Response({
+              'isSuccess': True,
+              'data': serializer.data,
+              'message': 'All ServiceMaster retrieved successfully',
+              'pagination': pagination.getPageInfo()
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def post(self, request):
+        try:
+          serializer = CreateServiceMasterSerializer(data=request.data)
+          if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'isSuccess': True,
+                'data': ServiceMasterSerializer(serializer.instance).data,
+                'message': 'ServiceMaster created successfully'
+            }, status=status.HTTP_201_CREATED)
+          else:
+            return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
     
-@api_view(['POST'])
-def createServiceMaster(request):
-    try:
-      serializer = CreateServiceMasterSerializer(data=request.data)
-      if serializer.is_valid():
-        serializer.save()
-        return Response({
-            'code': 200,
-            'success': True,
-            'data': ServiceMasterSerializer(serializer.instance).data,
-            'message': 'ServiceMaster created successfully'
-        })
-      else:
-        return Response({
-            'code': 400,
-            'success': False,
-            'data': None,
-            'message': 'Invalid inputs',
-            'errors': serializer.errors
-        })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
-@api_view(['PUT'])
-def updateServiceMaster(request, pk):
-    try:
-      serviceMaster = ServiceMaster.objects.get(id=pk)
-      serializer = CreateServiceMasterSerializer(instance=serviceMaster, data=request.data)
-      if serializer.is_valid():
-        serializer.save()
-        return Response({
-            'code': 200,
-            'success': True,
-            'data': ServiceMasterSerializer(serializer.instance).data,
-            'message': 'ServiceMaster updated successfully'
-        })
-      else:
-        return Response({
-            'code': 400,
-            'success': False,
-            'data': None,
-            'message': 'Invalid inputs',
-            'errors': serializer.errors
-        })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
-@api_view(['DELETE'])
-def deleteServiceMaster(request, pk):
-    try:
-      serviceMaster = ServiceMaster.objects.get(id=pk)
-      serviceMaster.delete()
-      return Response({
-          'code': 200,
-          'success': True,
-          'data': None,
-          'message': 'ServiceMaster deleted successfully'
-      })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
-# Service
-@api_view(['GET'])
-def getAllService(request):
-    try:
-      service = Service.objects.filter(deleted_at=None)
-      pagination = Pagination(service, request)
-      serializer = ServicesSerializer(pagination.getData(), many=True)
-      return Response({
-          'code': 200,
-          'success': True,
-          'data': serializer.data,
-          'message': 'All Service retrieved successfully',
-          'pagination': pagination.getPageInfo()
-      })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
-@api_view(['POST'])
-def createService(request):
-    try:
-      serializer = CreateServicesSerializer(data=request.data)
-      if serializer.is_valid():
-        service_master = ServiceMaster.objects.get(id=request.data['service_master'])
-        currency = Currency.objects.get(id=request.data['currency'])
-        package = Package.objects.get(id=request.data['package'])
-        serializer.save(service_master=service_master, package=package, currency=currency)
-        return Response({
-            'code': 200,
-            'success': True,
-            'date': ServicesSerializer(serializer.instance).data,
-            'message': 'Service created successfully'
-        })
-      else:
-        return Response({
-            'code': 400,
-            'success': False,
-            'data': None,
-            'message': 'Invalid inputs',
-            'errors': serializer.errors
-        })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
-@api_view(['PUT'])
-def updateService(request, pk):
-    try:
-      service = Service.objects.get(id=pk)
-      serializer = CreateServicesSerializer(instance=service, data=request.data)
-      if serializer.is_valid():
-        serializer.save()
-        return Response({
-            'code': 200,
-            'success': True,
-            'data': ServicesSerializer(serializer.instance).data,
-            'message': 'Service updated successfully'
-        })
-      else:
-        return Response({
-            'code': 400,
-            'success': False,
-            'data': None,
-            'message': 'Invalid inputs',
-            'errors': serializer.errors
-        })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
+class ServiceMasterDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return ServiceMaster.objects.get(pk=pk)
+        except ServiceMaster.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        try:
+          serviceMaster = self.get_object(pk)
+          serializer = ServiceMasterSerializer(serviceMaster)
+          return Response({
+              'isSuccess': True,
+              'data': serializer.data,
+              'message': 'ServiceMaster retrieved successfully'
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def put(self, request, pk):
+        try:
+          serviceMaster = self.get_object(pk)
+          serializer = CreateServiceMasterSerializer(instance=serviceMaster, data=request.data)
+          if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'isSuccess': True,
+                'data': ServiceMasterSerializer(serializer.instance).data,
+                'message': 'ServiceMaster updated successfully'
+            }, status=status.HTTP_200_OK)
+          else:
+            return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def delete(self, request, pk):
+        try:
+          serviceMaster = self.get_object(pk)
+          serviceMaster.delete()
+          return Response({
+              'isSuccess': True,
+              'data': None,
+              'message': 'ServiceMaster deleted successfully'
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
 
-@api_view(['DELETE'])
-def deleteService(request, pk):
-    try:
-      service = Service.objects.get(id=pk)
-      service.delete()
-      return Response({
-          'code': 200,
-          'success': True,
-          'data': None,
-          'message': 'Service deleted successfully'
-      })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
+# Service    
+class ServiceList(APIView):
+    def get(self, request):
+        try:
+          service = Service.objects.filter(deleted_at=None)
+          pagination = Pagination(service, request)
+          serializer = ServicesSerializer(pagination.getData(), many=True)
+          return Response({
+              'isSuccess': True,
+              'data': serializer.data,
+              'message': 'All Service retrieved successfully',
+              'pagination': pagination.getPageInfo()
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def post(self, request):
+        try:
+          serializer = CreateServicesSerializer(data=request.data)
+          if serializer.is_valid():
+            service_master = ServiceMaster.objects.get(id=request.data['service_master'])
+            currency = Currency.objects.get(id=request.data['currency'])
+            package = Package.objects.get(id=request.data['package'])
+            serializer.save(service_master=service_master, package=package, currency=currency)
+            return Response({
+                'isSuccess': True,
+                'data': ServicesSerializer(serializer.instance).data,
+                'message': 'Service created successfully'
+            }, status=status.HTTP_201_CREATED)
+          else:
+            return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+class ServiceDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Service.objects.get(pk=pk)
+        except Service.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        try:
+          service = self.get_object(pk)
+          serializer = ServicesSerializer(service)
+          return Response({
+              'isSuccess': True,
+              'data': serializer.data,
+              'message': 'Service retrieved successfully'
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def put(self, request, pk):
+        try:
+          service = self.get_object(pk)
+          serializer = CreateServicesSerializer(instance=service, data=request.data)
+          if serializer.is_valid():
+            service_master = ServiceMaster.objects.get(id=request.data['service_master'])
+            currency = Currency.objects.get(id=request.data['currency'])
+            package = Package.objects.get(id=request.data['package'])
+            serializer.save(service_master=service_master, package=package, currency=currency)
+            return Response({
+                'isSuccess': True,
+                'data': ServicesSerializer(serializer.instance).data,
+                'message': 'Service updated successfully'
+            }, status=status.HTTP_200_OK)
+          else:
+            return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def delete(self, request, pk):
+        try:
+          service = self.get_object(pk)
+          service.delete()
+          return Response({
+              'isSuccess': True,
+              'data': None,
+              'message': 'Service deleted successfully'
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
+            
 # Package
-@api_view(['GET'])
-def getAllPackage(request):
-    try:
-      package = Package.objects.filter(deleted_at=None)
-      pagination = Pagination(package, request)
-      serializer = PackageSerializer(pagination.getData(), many=True)
-      return Response({
-          'code': 200,
-          'success': True,
-          'data': serializer.data,
-          'message': 'All Package retrieved successfully',
-          'pagination': pagination.getPageInfo()
-      })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
+class PackageList(APIView):
+    def get(self, request):
+        try:
+          package = Package.objects.filter(deleted_at=None)
+          pagination = Pagination(package, request)
+          serializer = PackageSerializer(pagination.getData(), many=True)
+          return Response({
+              'isSuccess': True,
+              'data': serializer.data,
+              'message': 'All Package retrieved successfully',
+              'pagination': pagination.getPageInfo()
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def post(self, request):
+        try:
+          serializer = CreatePackageSerializer(data=request.data)
+          if serializer.is_valid():
+            influencer = User.objects.get(id=request.data['influencer'])
+            currency = Currency.objects.get(id=request.data['currency'])
+            serializer.save(influencer=influencer, currency=currency)
+            return Response({
+                'isSuccess': True,
+                'data': PackageSerializer(serializer.instance).data,
+                'message': 'Package created successfully'
+            }, status=status.HTTP_201_CREATED)
+          else:
+            return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
 
-@api_view(['POST'])
-def createPackage(request):
-    try:
-      serializer = CreatePackageSerializer(data=request.data)
-      if serializer.is_valid():
-        influencer = User.objects.get(id=request.data['influencer'])
-        currency = Currency.objects.get(id=request.data['currency'])
-        serializer.save(influencer=influencer, currency=currency)
-
-        return Response({
-            'code': 200,
-            'success': True,
-            'data': PackageSerializer(serializer.instance).data,
-            'message': 'Package created successfully'
-        })
-      else:
-        return Response({
-            'code': 400,
-            'success': False,
-            'data': None,
-            'message': 'Invalid inputs',
-            'errors': serializer.errors
-        })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
-@api_view(['PUT'])
-def updatePackage(request, pk):
-    try:
-      package = Package.objects.get(id=pk)
-      serializer = CreatePackageSerializer(instance=package, data=request.data)
-      if serializer.is_valid():
-        serializer.save()
-        return Response({
-            'code': 200,
-            'success': True,
-            'data': PackageSerializer(serializer.instance).data,
-            'message': 'Package updated successfully'
-        })
-      else:
-        return Response({
-            'code': 400,
-            'success': False,
-            'data': None,
-            'message': 'Invalid inputs',
-            'errors': serializer.errors
-        })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
-@api_view(['DELETE'])
-def deletePackage(request, pk):
-    try:
-      package = Package.objects.get(id=pk)
-      package.delete()
-      return Response({
-          'code': 200,
-          'success': True,
-          'data': None,
-          'message': 'Package deleted successfully'
-      })
-    except Exception as e:
-        print(e)
-        return Response({
-            'code': 500,
-            'success': False,
-            'data': None,
-            'message': 'Internal Server Error'
-        })
-    
+class PackageDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Package.objects.get(pk=pk)
+        except Package.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        try:
+          package = self.get_object(pk)
+          serializer = PackageSerializer(package)
+          return Response({
+              'isSuccess': True,
+              'data': serializer.data,
+              'message': 'Package retrieved successfully'
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def put(self, request, pk):
+        try:
+          package = self.get_object(pk)
+          serializer = CreatePackageSerializer(instance=package, data=request.data)
+          if serializer.is_valid():
+            influencer = User.objects.get(id=request.data['influencer'])
+            currency = Currency.objects.get(id=request.data['currency'])
+            serializer.save(influencer=influencer, currency=currency)
+            return Response({
+                'isSuccess': True,
+                'data': PackageSerializer(serializer.instance).data,
+                'message': 'Package updated successfully'
+            }, status=status.HTTP_200_OK)
+          else:
+            return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+        
+    def delete(self, request, pk):
+        try:
+          package = self.get_object(pk)
+          package.delete()
+          return Response({
+              'isSuccess': True,
+              'data': None,
+              'message': 'Package deleted successfully'
+          }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return handleServerException(e)
     
