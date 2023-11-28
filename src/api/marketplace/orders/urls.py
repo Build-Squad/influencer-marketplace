45322305a1f0ff044edf9e695f51078f76ab6d77
Parikshit85 <parikshit.s@navtech.io>
2023-11-28 13:01:38 +0530

    Add: Order attachment CRUD

--- src/api/marketplace/orders/urls.py
+++ src/api/marketplace/orders/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import OrderList, OrderDetail, OrderItemList, OrderItemDetail
+from .views import OrderList, OrderDetail, OrderItemList, OrderItemDetail, OrderAttachmentList, OrderAttachmentDetail
 
 urlpatterns = [
     path("order/", OrderList.as_view(), name="order-list"),
     path('order/<uuid:pk>/', OrderDetail.as_view(), name="order-details"),
 
-
     path("order-item/", OrderItemList.as_view(), name="order-item-list"),
     path('order-item/<uuid:pk>/', OrderItemDetail.as_view(), name="order-item-details"),
+
+    path("order-attachment/", OrderAttachmentList.as_view(), name="order-attachment-list"),
+    path('order-attachment/<uuid:pk>/', OrderAttachmentDetail.as_view(), name="order-attachment-details"),
 ]
--- src/api/marketplace/orders/views.py
+++ src/api/marketplace/orders/views.py
@@ -8,8 +8,8 @@ from marketplace.services import (
 from django.core.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Order, OrderItem
-from .serializers import OrderSerializer, OrderItemSerializer
+from .models import Order, OrderItem, OrderAttachment
+from .serializers import OrderSerializer, OrderItemSerializer, OrderAttachmentSerializer
 from rest_framework import status
 
 
@@ -218,6 +218,112 @@ class OrderItemDetail(APIView):
                     "isSuccess": True,
                     "data": None,
                     "message": "Order Item deleted successfully",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
+
+
+# ORDER-Attachment API-Endpoint
+# List-Create-API
+class OrderAttachmentList(APIView):
+    def get(self, request):
+        try:
+            orderAttachment = OrderAttachment.objects.all()
+            pagination = Pagination(orderAttachment, request)
+            serializer = OrderAttachmentSerializer(pagination.getData(), many=True)
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": serializer.data,
+                    "message": "All Order Attachments retrieved successfully",
+                    "pagination": pagination.getPageInfo(),
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
+
+    def post(self, request):
+        try:
+            serializer = OrderAttachmentSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "isSuccess": True,
+                        "data": OrderAttachmentSerializer(serializer.instance).data,
+                        "message": "Order Attachment created successfully",
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+
+
+# Retrieve-Update-Destroy API
+class OrderAttachmentDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return OrderAttachment.objects.get(pk=pk)
+        except OrderAttachment.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        try:
+            orderAttachment = self.get_object(pk)
+            if orderAttachment is None:
+                return handleNotFound("Order Attachment")
+            serializer = OrderAttachmentSerializer(orderAttachment)
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": serializer.data,
+                    "message": "Order Attachment retrieved successfully",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
+
+    def put(self, request, pk):
+        try:
+            orderAttachment = self.get_object(pk)
+            if orderAttachment is None:
+                return handleNotFound("Order Attachment")
+            serializer = OrderItemSerializer(instance=orderAttachment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "isSuccess": True,
+                        "data": OrderAttachmentSerializer(serializer.instance).data,
+                        "message": "Order Attachment updated successfully",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+
+    def delete(self, request, pk):
+        try:
+            orderAttachment = self.get_object(pk)
+            if orderAttachment is None:
+                return handleNotFound("Order Attachment")
+            try:
+                orderAttachment.delete()
+            except ValidationError as e:
+                return handleDeleteNotAllowed("Order Attachment")
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": None,
+                    "message": "Order Attachment deleted successfully",
                 },
                 status=status.HTTP_200_OK,
             )
