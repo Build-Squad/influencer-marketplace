29b71c126011cce0f1e3ab526c7447b249067e4b
Parikshit85 <parikshit.s@navtech.io>
2023-11-28 14:10:06 +0530

    Add: Category master CRUD

--- src/api/marketplace/accounts/urls.py
+++ src/api/marketplace/accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import TwitterAccountList, TwitterAccountDetail
+from .views import TwitterAccountList, TwitterAccountDetail, CategoryMasterList, CategoryMasterDetail
 
 urlpatterns = [
     path("twitter-account/", TwitterAccountList.as_view(), name="twitter-account-list"),
     path("twitter-account/<uuid:pk>/", TwitterAccountDetail.as_view(), name="twitter-account-detail"),
 
-    
+
+    path("category-master/", CategoryMasterList.as_view(), name="category-master-list"),
+    path("category-master/<uuid:pk>/", CategoryMasterDetail.as_view(), name="category-master-detail"),
+
+
 ]
--- src/api/marketplace/accounts/views.py
+++ src/api/marketplace/accounts/views.py
@@ -120,3 +120,110 @@ class TwitterAccountDetail(APIView):
         except Exception as e:
             return handleServerException(e)
 
+
+# Category Master API-Endpoint
+# List-Create-API
+class CategoryMasterList(APIView):
+    def get(self, request):
+        try:
+            categoryMaster = CategoryMaster.objects.all()
+            pagination = Pagination(categoryMaster, request)
+            serializer = CategoryMasterSerializer(pagination.getData(), many=True)
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": serializer.data,
+                    "message": "All Category Master retrieved successfully",
+                    "pagination": pagination.getPageInfo(),
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
+
+    def post(self, request):
+        try:
+            serializer = CategoryMasterSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "isSuccess": True,
+                        "data": CategoryMasterSerializer(serializer.instance).data,
+                        "message": "Category Master created successfully",
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+
+
+# Retrieve-Update-Destroy API
+class CategoryMasterDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return CategoryMaster.objects.get(pk=pk)
+        except CategoryMaster.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        try:
+            categoryMaster = self.get_object(pk)
+            if categoryMaster is None:
+                return handleNotFound("category Master")
+            serializer = CategoryMasterSerializer(categoryMaster)
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": serializer.data,
+                    "message": "Category Master retrieved successfully",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
+
+    def put(self, request, pk):
+        try:
+            categoryMaster = self.get_object(pk)
+            if categoryMaster is None:
+                return handleNotFound("Order")
+            serializer = CategoryMasterSerializer(
+                instance=categoryMaster, data=request.data
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "isSuccess": True,
+                        "data": CategoryMasterSerializer(serializer.instance).data,
+                        "message": "Category Master updated successfully",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+
+    def delete(self, request, pk):
+        try:
+            categoryMaster = self.get_object(pk)
+            if categoryMaster is None:
+                return handleNotFound("Category Master")
+            try:
+                categoryMaster.delete()
+            except ValidationError as e:
+                return handleDeleteNotAllowed("Category Master")
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": None,
+                    "message": "Category Master deleted successfully",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
