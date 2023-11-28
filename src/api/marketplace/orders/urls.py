65ea8cdb2e1a7cd531c7f2b2bb8a149292734960
Parikshit85 <parikshit.s@navtech.io>
2023-11-28 12:53:00 +0530

    Add: Order item CRUD

--- src/api/marketplace/orders/urls.py
+++ src/api/marketplace/orders/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import OrderList, OrderDetail
+from .views import OrderList, OrderDetail, OrderItemList, OrderItemDetail
 
 urlpatterns = [
     path("order/", OrderList.as_view(), name="order-list"),
     path('order/<uuid:pk>/', OrderDetail.as_view(), name="order-details"),
+
+
+    path("order-item/", OrderItemList.as_view(), name="order-item-list"),
+    path('order-item/<uuid:pk>/', OrderItemDetail.as_view(), name="order-item-details"),
 ]
--- src/api/marketplace/orders/views.py
+++ src/api/marketplace/orders/views.py
@@ -1,40 +1,53 @@
-from marketplace.services import Pagination, handleServerException, handleBadRequest, handleNotFound, handleDeleteNotAllowed
+from marketplace.services import (
+    Pagination,
+    handleServerException,
+    handleBadRequest,
+    handleNotFound,
+    handleDeleteNotAllowed,
+)
 from django.core.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, OrderItemSerializer
 from rest_framework import status
+
 
 # ORDER API-Endpoint
 # List-Create-API
-class OrderList(APIView):    
+class OrderList(APIView):
     def get(self, request):
         try:
-          orders = Order.objects.all()
-          pagination = Pagination(orders, request)
-          serializer = OrderSerializer(pagination.getData(), many=True)
-          return Response({
-              'isSuccess': True,
-              'data': serializer.data,
-              'message': 'All Order retrieved successfully',
-              'pagination': pagination.getPageInfo()
-          }, status=status.HTTP_200_OK)
+            orders = Order.objects.all()
+            pagination = Pagination(orders, request)
+            serializer = OrderSerializer(pagination.getData(), many=True)
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": serializer.data,
+                    "message": "All Order retrieved successfully",
+                    "pagination": pagination.getPageInfo(),
+                },
+                status=status.HTTP_200_OK,
+            )
         except Exception as e:
             return handleServerException(e)
 
     def post(self, request):
         try:
-          serializer = OrderSerializer(data=request.data)
-          if serializer.is_valid():
-            serializer.save()
-            return Response({
-                'isSuccess': True,
-                'data': OrderSerializer(serializer.instance).data,
-                'message': 'Order created successfully'
-            }, status=status.HTTP_201_CREATED)
-          else:
-            return handleBadRequest(serializer.errors)
+            serializer = OrderSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "isSuccess": True,
+                        "data": OrderSerializer(serializer.instance).data,
+                        "message": "Order created successfully",
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return handleBadRequest(serializer.errors)
         except Exception as e:
             return handleServerException(e)
 
@@ -49,50 +62,164 @@ class OrderDetail(APIView):
 
     def get(self, request, pk):
         try:
-          order = self.get_object(pk)
-          if order is None:
-            return handleNotFound('order')
-          serializer = OrderSerializer(order)
-          return Response({
-              'isSuccess': True,
-              'data': serializer.data,
-              'message': 'order retrieved successfully'
-          }, status=status.HTTP_200_OK)
+            order = self.get_object(pk)
+            if order is None:
+                return handleNotFound("order")
+            serializer = OrderSerializer(order)
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": serializer.data,
+                    "message": "order retrieved successfully",
+                },
+                status=status.HTTP_200_OK,
+            )
         except Exception as e:
             return handleServerException(e)
 
     def put(self, request, pk):
         try:
-          order = self.get_object(pk)
-          if order is None:
-            return handleNotFound('Order')
-          serializer = OrderSerializer(instance=order, data=request.data)
-          if serializer.is_valid():
-            serializer.save()
-            return Response({
-                'isSuccess': True,
-                'data': OrderSerializer(serializer.instance).data,
-                'message': 'Order updated successfully'
-            }, status=status.HTTP_200_OK)
-          else:
-            return handleBadRequest(serializer.errors)
+            order = self.get_object(pk)
+            if order is None:
+                return handleNotFound("Order")
+            serializer = OrderSerializer(instance=order, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "isSuccess": True,
+                        "data": OrderSerializer(serializer.instance).data,
+                        "message": "Order updated successfully",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return handleBadRequest(serializer.errors)
         except Exception as e:
             return handleServerException(e)
 
     def delete(self, request, pk):
         try:
-          order = self.get_object(pk)
-          if order is None:
-            return handleNotFound('Order')
-          try:
-            order.delete()
-          except ValidationError as e:
-            return handleDeleteNotAllowed('Order')
-          return Response({
-              'isSuccess': True,
-              'data': None,
-              'message': 'Order deleted successfully'
-          }, status=status.HTTP_200_OK)
+            order = self.get_object(pk)
+            if order is None:
+                return handleNotFound("Order")
+            try:
+                order.delete()
+            except ValidationError as e:
+                return handleDeleteNotAllowed("Order")
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": None,
+                    "message": "Order deleted successfully",
+                },
+                status=status.HTTP_200_OK,
+            )
         except Exception as e:
             return handleServerException(e)
-    
+
+
+# ORDER-Item API-Endpoint
+# List-Create-API
+class OrderItemList(APIView):
+    def get(self, request):
+        try:
+            orderItems = OrderItem.objects.all()
+            pagination = Pagination(orderItems, request)
+            serializer = OrderSerializer(pagination.getData(), many=True)
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": serializer.data,
+                    "message": "All Order Items retrieved successfully",
+                    "pagination": pagination.getPageInfo(),
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
+
+    def post(self, request):
+        try:
+            serializer = OrderItemSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "isSuccess": True,
+                        "data": OrderItemSerializer(serializer.instance).data,
+                        "message": "Order Item created successfully",
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+
+
+# Retrieve-Update-Destroy API
+class OrderItemDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return OrderItem.objects.get(pk=pk)
+        except OrderItem.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        try:
+            orderItem = self.get_object(pk)
+            if orderItem is None:
+                return handleNotFound("Order Item")
+            serializer = OrderItemSerializer(orderItem)
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": serializer.data,
+                    "message": "Order Item retrieved successfully",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
+
+    def put(self, request, pk):
+        try:
+            orderItem = self.get_object(pk)
+            if orderItem is None:
+                return handleNotFound("Order")
+            serializer = OrderItemSerializer(instance=orderItem, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "isSuccess": True,
+                        "data": OrderItemSerializer(serializer.instance).data,
+                        "message": "Order Item updated successfully",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return handleBadRequest(serializer.errors)
+        except Exception as e:
+            return handleServerException(e)
+
+    def delete(self, request, pk):
+        try:
+            orderItem = self.get_object(pk)
+            if orderItem is None:
+                return handleNotFound("Order Item")
+            try:
+                orderItem.delete()
+            except ValidationError as e:
+                return handleDeleteNotAllowed("Order Item")
+            return Response(
+                {
+                    "isSuccess": True,
+                    "data": None,
+                    "message": "Order Item deleted successfully",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return handleServerException(e)
