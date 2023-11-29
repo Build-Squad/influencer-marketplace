4acae0da30e500647f6b0f69194a613d13983f6c
Mudit Mahajan <mudit.m@navtech.io>
2023-11-29 12:13:43 +0530

    Add STATICFILES_DIRS to settings.py

--- src/api/marketplace/marketplace/settings.py
+++ src/api/marketplace/marketplace/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 AUTH_USER_MODEL = "accounts.User"
 ACCOUNT_AUTHENTICATION_METHOD = "email"
