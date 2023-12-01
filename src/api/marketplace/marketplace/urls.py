3211f990b63d39a7c91e53f80f87b8d226d180db
Parikshit85 <parikshit.s@navtech.io>
2023-12-01 13:04:07 +0530

    fix: Swagger bug fix and changed url to root

--- src/api/marketplace/marketplace/urls.py
+++ src/api/marketplace/marketplace/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path(
-        "api/docs/",
+        "",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
