abfff675f8986cae3b6901f810128498926a10a5
Mudit Mahajan <mudit.m@navtech.io>
2023-12-06 16:44:32 +0530

    Add getServicewithCredentials function

--- src/ui/src/services/httpServices.ts
+++ src/ui/src/services/httpServices.ts
@@ -42,6 +42,50 @@ const getService = async (url: string, params?: any) => {
   }
 };
 
+const getServicewithCredentials = async (url: string, params?: any) => {
+  try {
+    const response = await instance.get(url, {
+      params,
+      withCredentials: true,
+    });
+    if (response.data?.isSuccess) {
+      return {
+        isSuccess: response?.data?.isSuccess,
+        statusCode: response?.status,
+        data: response?.data?.data,
+        message: response?.data?.message,
+        errors: response?.data?.errors,
+      };
+    } else {
+      return {
+        isSuccess: response?.data?.isSuccess,
+        statusCode: response?.status,
+        data: response?.data?.data,
+        message: response?.data?.message,
+        errors: response?.data?.errors,
+      };
+    }
+  } catch (e: unknown) {
+    if (e instanceof AxiosError) {
+      return {
+        isSuccess: false,
+        statusCode: e?.response?.status,
+        data: null,
+        message: e?.response?.data?.error,
+        errors: null,
+      };
+    }
+    return {
+      isSuccess: false,
+      statusCode: 500,
+      data: null,
+      message: "Something went wrong, please try again later",
+      errors: null,
+    };
+  } finally {
+  }
+};
+
 const postService = async (url: string, data: unknown, params?: any) => {
   try {
     const response = await instance.post(url, data, { params });
@@ -164,4 +208,11 @@ const deleteService = async (url: string) => {
   }
 };
 
-export { getService, postService, deleteService, patchService, putService };
+export {
+  getService,
+  postService,
+  deleteService,
+  patchService,
+  putService,
+  getServicewithCredentials,
+};
