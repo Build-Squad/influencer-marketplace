22bb2e41f2b146e87da3931cd9b24ed29be9984d
Parikshit85 <parikshit.s@navtech.io>
2023-12-18 16:06:26 +0530

    test fixing twitter - rendering Django page for same domain

--- src/api/marketplace/marketplace/views.py
+++ src/api/marketplace/marketplace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from tweepy import Client, OAuth2UserHandler
 from django.http import (
     JsonResponse,
@@ -45,6 +45,25 @@ def logoutUser(request):
 
 def authTwitterUser(request):
     auth_url = oauth2_user_handler.get_authorization_url()
+    html_content = f"""
+        <html>
+        <body>
+            <h1>Hello, Django!</h1>
+            <button onclick="sendRequest('{auth_url}')">Send Request</button>
+            <script>
+                function sendRequest(url) {{
+                    // You can use the Fetch API or other methods to send a request
+                    // For simplicity, I'm using window.location.href to simulate a redirect
+                    window.location.href = url;
+                }}
+            </script>
+        </body>
+        </html>
+    """
+
+    # Return the HTML content using HttpResponse
+    return HttpResponse(html_content, content_type="text/html")
+
     return JsonResponse({"auth_url": auth_url})
 
 
@@ -66,93 +85,100 @@ def twitterLoginCallback(request):
                 "verified",
                 "created_at",
                 "location",
-                "url"
+                "url",
             ],
         ).data
-        existing_user = TwitterAccount.objects.filter(twitter_id=userData.id).first()
-
-        if existing_user is None:
-            newUser = TwitterAccount.objects.create(
-                twitter_id=userData.id,
-                name=userData.name,
-                user_name=userData.username,
-                access_token=access_token,
-                description=userData.description,
-                profile_image_url=userData.profile_image_url,
-                followers_count=userData.public_metrics["followers_count"],
-                following_count=userData.public_metrics["following_count"],
-                tweet_count=userData.public_metrics["tweet_count"],
-                listed_count=userData.public_metrics["listed_count"],
-                verified=userData.verified,
-                joined_at=userData.created_at,
-                location=userData.location,
-                url=userData.url
-            )
-
-            newUser.save()
-        else:
-            existing_user.access_token = access_token
-            # Update the user data
-            existing_user.name = userData.name
-            existing_user.user_name = userData.username
-            existing_user.description = userData.description
-            existing_user.profile_image_url = userData.profile_image_url
-            existing_user.followers_count = userData.public_metrics[
-                "followers_count"
-            ]
-            existing_user.following_count = userData.public_metrics[
-                "following_count"
-            ]
-            existing_user.tweet_count = userData.public_metrics["tweet_count"]
-            existing_user.listed_count = userData.public_metrics["listed_count"]
-            existing_user.verified = userData.verified
-            existing_user.joined_at = userData.created_at
-            existing_user.location = userData.location
-            existing_user.url = userData.url
-            existing_user.save()
-
-        current_twitter_user = TwitterAccount.objects.filter(
-            twitter_id=userData.id
-        ).first()
-        existing_user_account = User.objects.filter(
-            twitter_account=current_twitter_user
-        ).first()
-
-        if existing_user_account is None:
-            new_user_account = User.objects.create(
-                username=userData.username,
-                first_name=userData.name,
-                twitter_account_id=current_twitter_user.id,
-                role=Role.objects.filter(name="influencer").first(),
-            )
-            new_user_account.save()
-        else:
-            existing_user_account.last_login = datetime.datetime.now()
-            existing_user_account.save()
-
-        current_user = User.objects.filter(twitter_account=current_twitter_user).first()
-
-        # Creating a response object with JWT cookie
-        response = HttpResponseRedirect(
-            config("FRONT_END_URL") + "business/?authenticationStatus=success"
+        return JsonResponse(
+            {
+                "twitter_id": userData.id,
+                "name": userData.name,
+                "user_name": userData.username,
+                "access_token": access_token,
+                "description": userData.description,
+                "profile_image_url": userData.profile_image_url,
+            }
         )
+        # existing_user = TwitterAccount.objects.filter(twitter_id=userData.id).first()
 
-        # Convert the UUID to string
-        user_id = str(current_user.id)
-        # Payload for JWT token
-        payload = {
-            "id": user_id,
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=86400),
-            "iat": datetime.datetime.utcnow(),
-        }
-        # Set the JWT token in the response object
-        response = JWTOperations.setJwtToken(
-            res=response, cookie_name="jwt", payload=payload
-        )
+        # if existing_user is None:
+        #     newUser = TwitterAccount.objects.create(
+        #         twitter_id=userData.id,
+        #         name=userData.name,
+        #         user_name=userData.username,
+        #         access_token=access_token,
+        #         description=userData.description,
+        #         profile_image_url=userData.profile_image_url,
+        #         followers_count=userData.public_metrics["followers_count"],
+        #         following_count=userData.public_metrics["following_count"],
+        #         tweet_count=userData.public_metrics["tweet_count"],
+        #         listed_count=userData.public_metrics["listed_count"],
+        #         verified=userData.verified,
+        #         joined_at=userData.created_at,
+        #         location=userData.location,
+        #         url=userData.url,
+        #     )
 
-        return response
+        #     newUser.save()
+        # else:
+        #     existing_user.access_token = access_token
+        #     # Update the user data
+        #     existing_user.name = userData.name
+        #     existing_user.user_name = userData.username
+        #     existing_user.description = userData.description
+        #     existing_user.profile_image_url = userData.profile_image_url
+        #     existing_user.followers_count = userData.public_metrics["followers_count"]
+        #     existing_user.following_count = userData.public_metrics["following_count"]
+        #     existing_user.tweet_count = userData.public_metrics["tweet_count"]
+        #     existing_user.listed_count = userData.public_metrics["listed_count"]
+        #     existing_user.verified = userData.verified
+        #     existing_user.joined_at = userData.created_at
+        #     existing_user.location = userData.location
+        #     existing_user.url = userData.url
+        #     existing_user.save()
+
+        # current_twitter_user = TwitterAccount.objects.filter(
+        #     twitter_id=userData.id
+        # ).first()
+        # existing_user_account = User.objects.filter(
+        #     twitter_account=current_twitter_user
+        # ).first()
+
+        # if existing_user_account is None:
+        #     new_user_account = User.objects.create(
+        #         username=userData.username,
+        #         first_name=userData.name,
+        #         twitter_account_id=current_twitter_user.id,
+        #         role=Role.objects.filter(name="influencer").first(),
+        #     )
+        #     new_user_account.save()
+        # else:
+        #     existing_user_account.last_login = datetime.datetime.now()
+        #     existing_user_account.save()
+
+        # current_user = User.objects.filter(twitter_account=current_twitter_user).first()
+
+        # # Creating a response object with JWT cookie
+        # response = HttpResponseRedirect(
+        #     config("FRONT_END_URL") + "business/?authenticationStatus=success"
+        # )
+
+        # # Convert the UUID to string
+        # user_id = str(current_user.id)
+        # # Payload for JWT token
+        # payload = {
+        #     "id": user_id,
+        #     "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=86400),
+        #     "iat": datetime.datetime.utcnow(),
+        # }
+        # # Set the JWT token in the response object
+        # response = JWTOperations.setJwtToken(
+        #     res=response, cookie_name="jwt", payload=payload
+        # )
+
+        # return response
 
     except Exception as e:
+        return JsonResponse({"error": e})
         return HttpResponseRedirect(
             config("FRONT_END_URL") + "business/?authenticationStatus=error"
         )
