d84e41726408a7e7869b0f173f15c10d130993fc
Parikshit85 <parikshit.s@navtech.io>
2024-01-01 12:09:40 +0530

    minor: snackbar logout status fix

--- src/ui/app/layout.tsx
+++ src/ui/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
           status == "success" ? LOGIN_STATUS_SUCCESS : LOGIN_STATUS_FAILED,
       });
     }
-  }, [isTwitterUserLoggedIn]);
+  }, []);
 
   useEffect(() => {
     const status = params.get("authenticationStatus");
