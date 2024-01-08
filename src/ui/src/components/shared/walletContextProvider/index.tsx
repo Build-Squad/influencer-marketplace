974f2520a1d94612f6dc7a4d6c01ea3b713b494b
Mudit Mahajan <mudit.m@navtech.io>
2024-01-08 14:35:25 +0530

    Add WalletContextProvider component

---
+++ src/ui/src/components/shared/walletContextProvider/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import type { Adapter, WalletError } from "@solana/wallet-adapter-base";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useSnackbar } from "notistack";
+import type { ReactNode } from "react";
+import { useCallback, useMemo } from "react";
+
+export default function WalletContextProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(
+    () => [],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  );
+
+  const { enqueueSnackbar } = useSnackbar();
+  const onError = useCallback(
+    (error: WalletError, adapter?: Adapter) => {
+      enqueueSnackbar(
+        error.message ? `${error.name}: ${error.message}` : error.name,
+        { variant: "error" }
+      );
+      console.error(error, adapter);
+    },
+    [enqueueSnackbar]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} onError={onError} autoConnect={false}>
+        <WalletDialogProvider>{children}</WalletDialogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
