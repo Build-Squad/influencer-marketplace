99fb7f00ca4445abc26491513d3c345c1f6ac09b
Mudit Mahajan <mudit.m@navtech.io>
2024-01-10 17:03:21 +0530

    Add order_by field to OrderFilterType

--- src/ui/src/utils/types.ts
+++ src/ui/src/utils/types.ts
@@ -218,6 +218,7 @@ type OrderFilterType = {
   gt_rating?: string;
   lt_amount?: string;
   gt_amount?: string;
+  order_by?: string;
   objects?: {
     influencers?: UserType[];
     buyers?: UserType[];
