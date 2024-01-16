e67ca15420bbd608412bcb618835ecf5715b62f8
Parikshit85 <parikshit.s@navtech.io>
2024-01-16 17:17:49 +0530

    minor: change role from business to business_owner

--- src/ui/app/components/navbar/index.tsx
+++ src/ui/app/components/navbar/index.tsx
@@ -293,7 +293,7 @@ export default function Navbar({
           >
             {isTwitterUserLoggedIn ? (
               // Business menu items
-              user?.user?.role?.name.includes("business") ? (
+              user?.user?.role?.name.includes("business_owner") ? (
                 <MenuItemsComponent
                   items={[
                     "Home",
