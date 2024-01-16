53964602754e2accb076e6deed88712e7d36015f
Mudit Mahajan <mudit.m@navtech.io>
2024-01-16 13:13:14 +0530

    Refactor Checkout page

--- src/ui/app/business/checkout/page.tsx
+++ src/ui/app/business/checkout/page.tsx
@@ -1,38 +1,19 @@
 "use client";
 
+import CheckoutTable from "@/src/components/checkoutComponents/checkoutTable";
+import OrderItemForm from "@/src/components/checkoutComponents/orderItemForm";
 import { ConfirmCancel } from "@/src/components/shared/confirmCancel";
 import { notification } from "@/src/components/shared/notification";
 import { useAppDispatch, useAppSelector } from "@/src/hooks/useRedux";
-import {
-  initializeCart,
-  resetCart,
-  updateFieldValues,
-} from "@/src/reducers/cartSlice";
+import { initializeCart, resetCart } from "@/src/reducers/cartSlice";
 import {
   deleteService,
   postService,
   putService,
 } from "@/src/services/httpServices";
-import AddIcon from "@mui/icons-material/Add";
-import {
-  Box,
-  Button,
-  Divider,
-  FormLabel,
-  Grid,
-  Link,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { DateTimePicker } from "@mui/x-date-pickers";
+import { Box, Button, Grid, Link, Typography } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import dayjs from "dayjs";
 import NextLink from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -198,204 +179,7 @@ export default function CheckoutPage() {
         >
           {cart?.orderItems?.map((orderItem, index: number) => {
             return (
-              <Box
-                sx={{
-                  borderRadius: 4,
-                  backgroundColor: "#ffffff",
-                  boxShadow: "0px 4px 30px 0px rgba(0, 0, 0, 0.08)",
-                  width: "100%",
-                  p: 2,
-                  m: 2,
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                  }}
-                >
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {orderItem?.order_item?.package?.name}
-                  </Typography>
-                  <Button
-                    variant="outlined"
-                    color="secondary"
-                    sx={{
-                      borderRadius: 7,
-                    }}
-                    startIcon={<AddIcon />}
-                  >
-                    Add More
-                  </Button>
-                </Box>
-                <Divider
-                  sx={{
-                    my: 2,
-                  }}
-                />
-                <Grid
-                  container
-                  spacing={2}
-                  sx={{
-                    p: 2,
-                  }}
-                >
-                  {orderItem?.order_item?.order_item_meta_data
-                    .toSorted((a, b) => {
-                      return a?.order - b?.order;
-                    })
-                    ?.map((formFields) => {
-                      return (
-                        <Grid
-                          md={formFields?.span}
-                          lg={formFields?.span}
-                          xs={12}
-                          sm={12}
-                          sx={{
-                            my: 1,
-                            display: "flex",
-                            flexDirection: "column",
-                            p: 1,
-                          }}
-                        >
-                          <FormLabel
-                            sx={{
-                              color: "secondary.main",
-                            }}
-                          >
-                            {formFields?.label}
-                          </FormLabel>
-                          {formFields?.field_type === "text" && (
-                            <TextField
-                              value={formFields?.value}
-                              name={formFields?.id}
-                              onChange={(e) => {
-                                dispatch(
-                                  updateFieldValues({
-                                    index: index,
-                                    service_master_meta_data_id:
-                                      formFields?.service_master_meta_data_id
-                                        ? formFields?.service_master_meta_data_id
-                                        : "",
-                                    order_item_meta_data_id: formFields?.id,
-                                    value: e.target.value,
-                                  })
-                                );
-                              }}
-                              variant="outlined"
-                              fullWidth
-                              placeholder={formFields?.placeholder}
-                              size="small"
-                              sx={{
-                                "& .MuiOutlinedInput-root": {
-                                  borderRadius: 3,
-                                },
-                              }}
-                            />
-                          )}
-                          {formFields?.field_type === "long_text" && (
-                            <TextField
-                              name={formFields?.id}
-                              value={formFields?.value}
-                              onChange={(e) => {
-                                dispatch(
-                                  updateFieldValues({
-                                    index: index,
-                                    service_master_meta_data_id:
-                                      formFields?.service_master_meta_data_id
-                                        ? formFields?.service_master_meta_data_id
-                                        : "",
-                                    order_item_meta_data_id: formFields?.id,
-                                    value: e.target.value,
-                                  })
-                                );
-                              }}
-                              variant="outlined"
-                              fullWidth
-                              multiline
-                              rows={4}
-                              inputProps={{
-                                maxLength: formFields?.max,
-                              }}
-                              placeholder={formFields?.placeholder}
-                              size="small"
-                              sx={{
-                                "& .MuiOutlinedInput-root": {
-                                  borderRadius: 3,
-                                },
-                              }}
-                            />
-                          )}
-                          {formFields?.field_type === "date_time" && (
-                            <DateTimePicker
-                              value={
-                                dayjs(formFields?.value).isValid()
-                                  ? dayjs(formFields?.value)
-                                  : null
-                              }
-                              onChange={(e) => {
-                                dispatch(
-                                  updateFieldValues({
-                                    index: index,
-                                    service_master_meta_data_id:
-                                      formFields?.service_master_meta_data_id
-                                        ? formFields?.service_master_meta_data_id
-                                        : "",
-                                    order_item_meta_data_id: formFields?.id,
-                                    value: dayjs(e).format(
-                                      "YYYY-MM-DD HH:mm:ss"
-                                    ),
-                                  })
-                                );
-                              }}
-                              slotProps={{
-                                textField: {
-                                  size: "small",
-                                  variant: "outlined",
-                                  fullWidth: true,
-                                  // borderRadius: 3,
-                                  name: formFields?.id,
-                                  sx: {
-                                    "& .MuiOutlinedInput-root": {
-                                      borderRadius: 3,
-                                    },
-                                  },
-                                },
-                              }}
-                            />
-                          )}
-                          {formFields?.field_type === "media" && (
-                            <Box
-                              sx={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                                alignItems: "center",
-                              }}
-                            >
-                              <Button
-                                variant="contained"
-                                disableElevation
-                                sx={{
-                                  p: 2,
-                                  mt: 1,
-                                  borderRadius: 8,
-                                }}
-                              >
-                                {formFields?.label}
-                              </Button>
-                            </Box>
-                          )}
-                        </Grid>
-                      );
-                    })}
-                </Grid>
-              </Box>
+              <OrderItemForm key={index} orderItem={orderItem} index={index} />
             );
           })}
           <Grid item xs={12}>
@@ -406,19 +190,6 @@ export default function CheckoutPage() {
                 alignItems: "center",
               }}
             >
-              {/* <Button
-                variant="outlined"
-                color="secondary"
-                sx={{
-                  p: 1,
-                  mt: 1,
-                  borderRadius: 8,
-                  minWidth: 100,
-                  mx: 2,
-                }}
-              >
-                Clear
-              </Button> */}
               <Button
                 variant="contained"
                 color="secondary"
@@ -503,72 +274,7 @@ export default function CheckoutPage() {
                 overflow: "auto",
               }}
             >
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell
-                      sx={{
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Service
-                    </TableCell>
-                    <TableCell
-                      sx={{
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Quantity
-                    </TableCell>
-                    <TableCell
-                      sx={{
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Amount
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {cart?.orderItems?.length === 0 && (
-                    <TableRow>
-                      <TableCell
-                        colSpan={12}
-                        sx={{
-                          textAlign: "center",
-                        }}
-                      >
-                        No service added
-                      </TableCell>
-                    </TableRow>
-                  )}
-                  {cart?.servicesAdded.map((orderItem) => (
-                    <TableRow key={orderItem?.item.id}>
-                      <TableCell>{orderItem?.item?.package?.name}</TableCell>
-                      <TableCell>{orderItem?.quantity} </TableCell>
-                      <TableCell>
-                        {Number(orderItem?.platform_price)?.toFixed(2)}{" "}
-                        {orderItem?.item?.currency?.symbol}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                  <TableRow>
-                    {/* Total */}
-                    <TableCell
-                      sx={{
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Total
-                    </TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>
-                      {cart?.orderTotal?.toFixed(2)}{" "}
-                      {cart?.orderTotalCurrency?.symbol}
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
+              <CheckoutTable />
             </Box>
             <Box
               sx={{
