24323e454152d6b83f36732a917dc02869cdd940
Mudit Mahajan <mudit.m@navtech.io>
2024-01-24 16:57:39 +0530

    Remove unused import

--- src/api/marketplace/accounts/serializers.py
+++ src/api/marketplace/accounts/serializers.py
@@ -1,7 +1,6 @@
 from unicodedata import category
 from marketplace.services import truncateWalletAddress
 from rest_framework import serializers
-from uuid import UUID
 
 from core.serializers import LanguageMasterSerializer
 from orders.models import Order, OrderItem
