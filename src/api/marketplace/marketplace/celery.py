bce4c2fbb53dde245358219d0fda3ad7cf95bad3
Mudit Mahajan <mudit.m@navtech.io>
2024-01-29 19:15:03 +0530

    Update Celery configuration

--- src/api/marketplace/marketplace/celery.py
+++ src/api/marketplace/marketplace/celery.py
@@ -6,7 +6,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
 
-app = Celery('marketplace', backend='redis')
+app = Celery('marketplace')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
