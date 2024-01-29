141f25fb27461828923257a2dbb84bb386b4e5f8
Mudit Mahajan <mudit.m@navtech.io>
2024-01-29 22:03:32 +0530

    Update Celery configuration

--- src/api/marketplace/marketplace/settings.py
+++ src/api/marketplace/marketplace/settings.py
@@ -246,8 +246,8 @@ USE_L10N = True
 USE_TZ = True
 
 # Celery
-CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:6379')
-CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default='redis://localhost:6379')
+CELERY_BROKER_URL = config('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
