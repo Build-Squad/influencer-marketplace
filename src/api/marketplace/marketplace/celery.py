7dd4f4b21120b0d529326483740288c2c750aed4
Parikshit85 <parikshit.s@navtech.io>
2024-01-31 13:22:28 +0530

    Add: Refresh token update every 2 hour

--- src/api/marketplace/marketplace/celery.py
+++ src/api/marketplace/marketplace/celery.py
@@ -2,13 +2,23 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marketplace.settings")
 
-app = Celery('marketplace')
+app = Celery("marketplace")
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    # Update access token every 2 hour
+    "update-access-tokens": {
+        "task": "accounts.tasks.updateAccessTokens",
+        "schedule": crontab(hour="*/2"),
+    },
+}
