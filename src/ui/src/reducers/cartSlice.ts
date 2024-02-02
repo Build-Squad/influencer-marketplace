c3ae7f6615dbb29965bf86c6a55c1842316cd635
Mudit Mahajan <mudit.m@navtech.io>
2024-02-02 16:03:50 +0530

    Add influencer and buyer wallets to cart state

--- src/ui/src/reducers/cartSlice.ts
+++ src/ui/src/reducers/cartSlice.ts
@@ -24,6 +24,8 @@ type CartState = {
   servicesAdded: ServiceAdded[];
   orderTotal: number;
   orderTotalCurrency: CurrencyType;
+  influencer_wallet?: WalletType;
+  buyer_wallet?: WalletType;
 };
 
 type AddOrderItemPayloadType = {
@@ -195,6 +197,8 @@ export const cartSlice = createSlice({
       // If there are no more order items, reset influencer, orderTotal, and orderTotalCurrency here
       if (state.orderItems.length === 0) {
         state.influencer = null;
+        state.influencer_wallet = undefined;
+        state.buyer_wallet = undefined;
         state.orderTotal = 0;
         state.orderTotalCurrency = {
           id: "",
@@ -248,6 +252,8 @@ export const cartSlice = createSlice({
     resetCart: (state) => {
       state.orderId = null;
       state.influencer = null;
+      state.influencer_wallet = undefined;
+      state.buyer_wallet = undefined;
       state.orderItems = [];
       state.servicesAdded = [];
       state.orderTotal = 0;
@@ -266,10 +272,14 @@ export const cartSlice = createSlice({
         orderId: string;
         influencer: UserType | null;
         orderItems: OrderItemType[];
+        influencer_wallet?: WalletType;
+        buyer_wallet?: WalletType;
       }>
     ) => {
       state.orderId = action.payload.orderId;
       state.influencer = action.payload.influencer;
+      state.influencer_wallet = action.payload.influencer_wallet;
+      state.buyer_wallet = action.payload.buyer_wallet;
       state.orderItems = action.payload.orderItems.map((item) => {
         return {
           order_item: {
@@ -343,7 +353,7 @@ export const cartSlice = createSlice({
     ) => {
       const orderItem = state.orderItems[action.payload.index];
       orderItem.publish_date = action.payload.value;
-    }
+    },
   },
 });
 
