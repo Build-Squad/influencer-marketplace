375104e4afd50f309483eecb0dcc896a488d7320
Mudit Mahajan <mudit.m@navtech.io>
2024-02-12 18:51:08 +0530

    Add key prop to mapped elements in elevateSection and faqSection components

--- src/ui/app/influencer/components/elevateSection/index.tsx
+++ src/ui/app/influencer/components/elevateSection/index.tsx
@@ -187,6 +187,7 @@ export default function ElevateSection({}: Props) {
                   onClick={() => {
                     handleSelect({ index });
                   }}
+                  key={index}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
