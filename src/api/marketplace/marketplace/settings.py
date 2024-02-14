cfa35b6b0afd872cf664da964a43ac94988aaceb
Mudit Mahajan <mudit.m@navtech.io>
2024-02-14 17:12:43 +0530

    Add template configuration to settings.py

--- src/api/marketplace/marketplace/settings.py
+++ src/api/marketplace/marketplace/settings.py
@@ -80,6 +80,22 @@ LOGGING = {
     }
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'marketplace/templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
