1d4c25d5a051906dc09c6982364ced6a53ae2443
Ruben Colomina <rcolomina@gmail.com>
2024-02-15 04:27:58 +0000

    add platform config to launch validate instruction

--- solana-python/launch_validate_escrow.py
+++ solana-python/launch_validate_escrow.py
@@ -9,8 +9,17 @@ from utils import load_configuration
 
 
 def main():
+    configuration = load_configuration()
+    network = configuration["network"]
+    program_id = configuration["program_id_testnet"] if configuration["network"] == "testnet" else configuration["program_id_devnet"]
+    PROGRAM_ID = Pubkey.from_string(program_id)
+    print(f"Network: {network} Program ID: {program_id}")
+
+    path_to_influencer_keypair = "platform.json"
+    platform, platform_pk = get_local_keypair_pubkey(path=path_to_bussines_keypair)
+    
     # implement caller to validate escrow instruction
-    pass
+    # ....    
 
 
 if __name__ == "__main__":
