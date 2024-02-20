e155c1e0bb2b9fd18af0ce125d1e91fc0c9cdb86
Mudit Mahajan <mudit.m@navtech.io>
2024-02-20 13:04:45 +0530

    Add wallet disconnection functionality

--- src/ui/src/components/walletsTable/index.tsx
+++ src/ui/src/components/walletsTable/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Chip, Typography } from "@mui/material";
+import { Box, Chip, IconButton, Tooltip, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import {
   TableRow,
@@ -13,6 +13,10 @@ import { getService } from "@/src/services/httpServices";
 import { notification } from "../shared/notification";
 import EmptyWalletIcon from "@/public/svg/No_wallets_connected.svg";
 import Image from "next/image";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { useAppSelector } from "@/src/hooks/useRedux";
+import useTwitterAuth from "@/src/hooks/useTwitterAuth";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 
 type Props = {
   walletOpen: boolean;
@@ -35,7 +39,15 @@ const styles = {
 };
 
 export default function WalletsTable({ walletOpen }: Props) {
+  const user = useAppSelector((state) => state.user);
+  const { logoutTwitterUser } = useTwitterAuth();
+  const { publicKey, disconnect } = useWallet();
   const [userWallets, setUserWallets] = useState<walletsType>();
+  const [wallets, setWallets] = useState<WalletType[]>([]);
+  const [connectedWallet, setConnectedWallet] = useState<WalletType | null>(
+    null
+  );
+
   const getUserWallets = async () => {
     const { isSuccess, data, message } = await getService("/account/wallets/");
     if (isSuccess) {
@@ -49,10 +61,45 @@ export default function WalletsTable({ walletOpen }: Props) {
         };
       });
       setUserWallets(wallets);
+      setWallets(data?.data);
     } else {
       notification(message ? message : "Something went wrong", "error");
     }
   };
+
+  const disConnectWallet = async () => {
+    try {
+      if (connectedWallet?.wallet_address_id === user?.user?.username) {
+        await logoutTwitterUser();
+        return;
+      }
+      await disconnect();
+      notification("Wallet has been disconnected");
+    } catch (error) {
+      notification("Error disconnecting wallet " + error, "error");
+    }
+  };
+
+  useEffect(() => {
+    if (publicKey) {
+      console.log("publicKey", publicKey?.toBase58(), wallets);
+      let concatenatedPublicKey = publicKey?.toBase58();
+      concatenatedPublicKey =
+        concatenatedPublicKey.slice(0, 4) +
+        "..." +
+        concatenatedPublicKey.slice(-4);
+      const connectedWallet = wallets.find(
+        (wallet) => wallet.wallet_address_id === concatenatedPublicKey
+      );
+      if (connectedWallet) {
+        setConnectedWallet(connectedWallet);
+      } else {
+        setConnectedWallet(null);
+      }
+    } else {
+      setConnectedWallet(null);
+    }
+  }, [publicKey]);
 
   useEffect(() => {
     getUserWallets();
@@ -79,10 +126,20 @@ export default function WalletsTable({ walletOpen }: Props) {
               </TableCell>
               <TableCell
                 align="left"
-                sx={{ ...styles.headerCellStyle, borderLeft: "0 !important" }}
+                sx={{
+                  ...styles.headerCellStyle,
+                  borderLeft: "0 !important",
+                  borderRight: "0 !important",
+                }}
               >
                 Wallet Name
               </TableCell>
+              <TableCell
+                sx={{
+                  ...styles.headerCellStyle,
+                  borderLeft: "0 !important",
+                }}
+              ></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -102,13 +159,26 @@ export default function WalletsTable({ walletOpen }: Props) {
                     {row.isPrimary ? (
                       <Chip
                         sx={{ ml: 1, backgroundColor: "#9AE3E9" }}
-                        label="primary"
+                        label="Primary"
                         size="small"
                       />
                     ) : null}
                   </TableCell>
                   <TableCell align="left" sx={styles.bodyCellStyle}>
                     {row.walletName}
+                  </TableCell>
+                  <TableCell>
+                    {connectedWallet?.wallet_address_id === row.addr && (
+                      <Tooltip title="Disconnect Wallet">
+                        <IconButton
+                          onClick={() => {
+                            disConnectWallet();
+                          }}
+                        >
+                          <RemoveCircleOutlineIcon color="error" />
+                        </IconButton>
+                      </Tooltip>
+                    )}
                   </TableCell>
                 </TableRow>
               ))
