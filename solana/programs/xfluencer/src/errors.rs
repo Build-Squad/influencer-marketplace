cb1edcae6045fb36266ada2076ce344efbc23d2b
Ruben Colomina <rcolomina@gmail.com>
2024-02-21 06:54:40 +0000

    add new custom error messages

--- solana/programs/xfluencer/src/errors.rs
+++ solana/programs/xfluencer/src/errors.rs
@@ -6,4 +6,18 @@ pub enum CustomError {
     CannotClaim,
     #[msg("Already claim")]
     AlreadyClaim,  
+    #[msg("Escrow already cancel for business")]
+    EscrowAlreadyCancel,  
+    #[msg("Escrow already released for influencer")]
+    EscrowAlreadyReleased,      
+    #[msg("Bad Target State for Escrow (1) for cancel, (2) for release")]
+    BadTargetStateForEscrow,  
+    #[msg("Missmatch business public key")]
+    MissmatchBusiness,
+    #[msg("Missmatch influencer publick key")]
+    MissmatchInfluencer,
+    #[msg("Bad Escrow State")]
+    BadEscrowState,
+    #[msg("Missmatch Authority")]
+    MissmatchAuthority,
 }
