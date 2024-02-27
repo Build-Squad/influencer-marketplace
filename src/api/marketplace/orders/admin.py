6ccfaac5b19258fd5cbff3e7d23d9bc1840aad23
Mudit Mahajan <mudit.m@navtech.io>
2024-02-27 11:18:57 +0530

    Add admin models for OrderMessage and Review

--- src/api/marketplace/orders/admin.py
+++ src/api/marketplace/orders/admin.py
@@ -25,13 +25,21 @@ class OrderTrackingAdmin(admin.ModelAdmin):
 class OrderItemTrackingAdmin(admin.ModelAdmin):
     list_display = [field.name for field in OrderItemTracking._meta.fields]
 
+
+class OrderMessageAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in OrderMessage._meta.fields]
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Review._meta.fields]
+
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(OrderAttachment)
 admin.site.register(OrderItemTracking, OrderItemTrackingAdmin)
-admin.site.register(OrderMessage)
+admin.site.register(OrderMessage, OrderMessageAdmin)
 admin.site.register(OrderMessageAttachment)
 admin.site.register(Transaction, TransactionAdmin)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(OrderItemMetaData, OrderItemMetaDataAdmin)
 admin.site.register(OrderTracking, OrderTrackingAdmin)
