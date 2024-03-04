2b1c74bb56ad48c2ad286f21327f64821e28aed0
Ruben Colomina <rcolomina@gmail.com>
2024-03-04 08:31:34 +0000

    add lamports transfer within validation ix

--- solana/programs/xfluencer/src/lib.rs
+++ solana/programs/xfluencer/src/lib.rs
@@ -195,12 +195,24 @@ pub mod xfluencer {
             let escrow_amount: u64 = ctx.accounts.escrow_account.get_lamports();
             
             let fees_amount: u64 = match mul_div_u64(escrow_amount, percentage_fee as u64, 10000 as u64) {
-                Some(fees_amount) => fees_amount,
+                Some(fees_amount) => {
+                    if escrow_amount > fees_amount {
+                       return err!(CustomError::NumericalProblemFoundCalculatingFees)
+                    }
+                    else {
+                        fees_amount
+                    }
+                },
                 None => {
                     return err!(CustomError::NumericalProblemFoundCalculatingFees)
                 }
             };
 
+            let from_account = ctx.accounts.escrow_account.to_account_info();
+            let to_account = ctx.accounts.validation_authority.to_account_info();
+
+            **from_account.try_borrow_mut_lamports()? -= fees_amount;
+            **to_account.try_borrow_mut_lamports()? += fees_amount; 
             
         }
 
