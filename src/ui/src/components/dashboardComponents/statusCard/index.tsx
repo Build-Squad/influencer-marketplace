18ebbe3fe8c2e27f303fce12b3322109c0454caf
Mudit Mahajan <mudit.m@navtech.io>
2024-03-15 13:19:59 +0530

    Add description tooltip to status card component

--- src/ui/src/components/dashboardComponents/statusCard/index.tsx
+++ src/ui/src/components/dashboardComponents/statusCard/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Tooltip, Typography } from "@mui/material";
 import React from "react";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 type StatusCardProps = {
   card: {
@@ -11,13 +12,15 @@ type StatusCardProps = {
     onClick: () => void;
   };
   selectedCard: number;
-  count: number | string;
+  count: number | string | null;
+  description?: string;
 };
 
 export default function StatusCard({
   card,
   selectedCard,
   count,
+  description,
 }: StatusCardProps) {
   return (
     <Box
@@ -31,7 +34,7 @@ export default function StatusCard({
         borderRadius: 4,
         boxShadow: "0px 4px 31px 0px rgba(0, 0, 0, 0.08)",
         backgroundColor: selectedCard === card?.value ? "#000" : "#fff",
-        minHeight: "180px",
+        minHeight: count ? "180px" : "",
       }}
       onClick={card.onClick}
     >
@@ -48,18 +51,40 @@ export default function StatusCard({
         sx={{
           display: "flex",
           alignItems: "center",
+          justifyContent: "space-between",
+          width: "100%",
         }}
       >
-        {card.icon}
-        <Typography
-          variant="body1"
+        <Box
           sx={{
-            color: card?.value === selectedCard ? "#fff" : "#000",
-            ml: 1,
+            display: "flex",
+            alignItems: "center",
           }}
         >
-          {card.label}
-        </Typography>
+          {card.icon}
+          <Typography
+            variant="h6"
+            sx={{
+              color: card?.value === selectedCard ? "#fff" : "#000",
+              ml: 1,
+            }}
+          >
+            {card.label}
+          </Typography>
+        </Box>
+        {description ? (
+          <Tooltip title={description}>
+            <IconButton>
+              <InfoOutlinedIcon
+                sx={{
+                  color: card?.value === selectedCard ? "#fff" : "#000",
+                }}
+              />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Box></Box>
+        )}
       </Box>
     </Box>
   );
