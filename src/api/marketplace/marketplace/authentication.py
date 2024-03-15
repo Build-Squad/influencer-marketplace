03cea866b9b3fde9c70e0e0cff7c23d9ee893523
Mudit Mahajan <mudit.m@navtech.io>
2024-03-15 17:26:21 +0530

    Add JWTAuthenticationOptional class for optional JWT authentication and add 'is_bookmarked' field to TwitterAccountType

--- src/api/marketplace/marketplace/authentication.py
+++ src/api/marketplace/marketplace/authentication.py
@@ -30,3 +30,33 @@ class JWTAuthentication(BaseAuthentication):
             request.token = token
         except Exception as e:
             raise exceptions.AuthenticationFailed(e)
+
+# Only attach the User object to the request object if the token is valid otherwise pass the request object as is and dont raise any exceptions
+
+
+class JWTAuthenticationOptional(BaseAuthentication):
+    def authenticate(self, request):
+        try:
+            payload, token = JWTOperations.getPayload(
+                req=request, cookie_name="jwt")
+
+            # Check for the expiration of the token
+            if JWTOperations.isTokenExpired(payload):
+                return request, None
+
+            # Extract user ID from the payload
+            user_id = payload.get("id")
+            if not user_id:
+                return request, None
+
+            # Retrieve the corresponding user object
+            try:
+                userAccount = User.objects.get(id=user_id)
+            except userAccount.DoesNotExist:
+                return request, None
+
+            request.user_account = userAccount
+            request.token = token
+            return request, None
+        except Exception as e:
+            return request, None
