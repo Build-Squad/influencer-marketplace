eb3e511a8b00a856e982dbcb6f1091ec9b163430
Ruben Colomina <rcolomina@gmail.com>
2024-03-29 16:49:31 +0000

    set vault autority to validation autority transfering spl tokens to vault

--- solana/programs/xfluencer/src/processor/initialize_escrow.rs
+++ solana/programs/xfluencer/src/processor/initialize_escrow.rs
@@ -3,7 +3,8 @@ use anchor_lang::{
 };
 use spl_token::instruction::AuthorityType;
 
-use anchor_spl::token;
+use anchor_spl::token::{self, SetAuthority,  Transfer};
+
 
 use crate::CreateEscrow;
 
@@ -12,41 +13,49 @@ pub fn process(ctx: Context<CreateEscrow>,
     _vault_account_bump: u8,
     amount: u64,
     order_code: u64) -> ProgramResult {
-   
-    let clock: Clock = Clock::get().unwrap();
-    
+
     ctx.accounts.escrow_account.business_key = *ctx.accounts.business.key;
-    ctx.accounts.escrow_account.business_deposit_token_account = *ctx.accounts.business_deposit_token_account.to_account_info().key;
+
+    ctx.accounts.escrow_account.business_deposit_token_account 
+        = *ctx.accounts.business_deposit_token_account.to_account_info().key;
+
     ctx.accounts.escrow_account.influencer_key = *ctx.accounts.influencer.key;
-    ctx.accounts.escrow_account.influencer_receive_token_account = *ctx.accounts.influencer_receive_token_account.to_account_info().key;
-    ctx.accounts.escrow_account.validation_authority = *ctx.accounts.validation_authority.key;
+
+    ctx.accounts.escrow_account.influencer_receive_token_account 
+        = *ctx.accounts.influencer_receive_token_account.to_account_info().key;
+
+    ctx.accounts.escrow_account.validation_authority 
+        = *ctx.accounts.validation_authority.key;
+
     ctx.accounts.escrow_account.amount = amount;
-    ctx.accounts.escrow_account.order_code = order_code;    
-    ctx.accounts.escrow_account.delivery_time = clock.unix_timestamp;
+    ctx.accounts.escrow_account.order_code = order_code;
     ctx.accounts.escrow_account.status = 0;
-     
-    msg!("Amount of Tokens to store in SPL Escrow: {}", amount);
+    
 
-    let escrow_seed: String = format!("{}{}", "escrow".to_string(), order_code.to_string());
-    let escrow_pda_seed: &[u8] = escrow_seed.as_bytes();
-
-    // Set the Vault Authority to the Escrow PDA
-    let (vault_authority, _vault_authority_bump) 
-        = Pubkey::find_program_address(&[escrow_pda_seed], ctx.program_id);
-    msg!("Vault Authority {}",vault_authority);
-
-    // Set the Authority of the Vault to the Escrow PDA 
     token::set_authority(
-        ctx.accounts.into_set_authority_context(),
+        CpiContext::new(
+            ctx.accounts.token_program.clone(), 
+            SetAuthority {
+                account_or_mint: ctx.accounts.vault_account.to_account_info().clone(),
+                current_authority: ctx.accounts.business.clone(),
+            }
+        ),      
         AuthorityType::AccountOwner,
-        Some(vault_authority),
+        Some(ctx.accounts.escrow_account.validation_authority),
     )?;
 
-    // Transfer Tokens to the Vault
     token::transfer(
-        ctx.accounts.into_transfer_to_pda_context(),
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer {
+                from: ctx.accounts.business_deposit_token_account.to_account_info().clone(),
+                to: ctx.accounts.vault_account.to_account_info().clone(),
+                authority: ctx.accounts.business.clone(),
+            },
+        ),
         ctx.accounts.escrow_account.amount,
     )?;
+
 
     Ok(())
 }
