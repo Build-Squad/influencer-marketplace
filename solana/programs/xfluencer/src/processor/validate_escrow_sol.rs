3e55573574445ab392ab84336455ccbb490561de
Ruben Colomina <rcolomina@gmail.com>
2024-04-09 08:55:49 +0100

    fix fees calculation on SOL based escrows

--- solana/programs/xfluencer/src/processor/validate_escrow_sol.rs
+++ solana/programs/xfluencer/src/processor/validate_escrow_sol.rs
@@ -47,6 +47,13 @@ pub fn process(
 
     ctx.accounts.escrow_account.status = target_state;
 
+    if target_state == 1 {
+        msg!(
+            "Escrow has changed its state to cancel (1)"
+        );
+
+    }
+
     // in case of state is delivered, transfer funds to the validation_authority
     if target_state == 2 {
         msg!(
@@ -54,14 +61,69 @@ pub fn process(
             &percentage_fee.to_string()
         );
 
-        if percentage_fee > 1000 {
-            // 10 %
+        if percentage_fee > 500 {
+            // 5 % 
             return err!(CustomError::PercentageFeeOutOfrange)?;
         }
 
         let escrow_amount: u64 = ctx.accounts.escrow_account.get_lamports();
 
-        let fees_amount: u64 = match mul_div_u64(escrow_amount, percentage_fee as u64, 10000 as u64)
+        /* 
+
+        Assuming the amount passed to the escrow already includes the fees:
+
+        We need to break down this total amount into two parts: the payment amount for service X, 
+        
+        and the fee (denoted as 'r') set by the platform for service X. 
+        
+        Therefore, the input amount sent to the escrow (denoted as Y) will be composed as follows:
+
+        Y = X + r * X,
+
+        where:
+        - 'r' represents the fee (0 <= r <= 0.05) set by the platform.
+        - 'X' is the amount to be given to the influencer.
+
+        Given the total escrow amount (Y) and the fee 'r', how do we determine the fee (denoted as 'F') 
+        
+        applied to the payment for service X that the contract should send to the authority?
+
+        Let's factor out the common term 'X':
+
+        Y = X + r * X,
+
+        which simplifies to:
+
+        Y = X * (1 + r).
+
+        This implies that the service amount 'X' can be calculated as:
+
+        X = Y / (1 + r).
+
+        Hence, the fee applied to the payment is:
+
+        F = X * r = Y * r / (1 + r).
+
+        The amount 'F' represents what the contract will remit to the authority 
+        
+        for each successfully validated escrow release.
+
+        It's important to note that in Rust, we will utilize lossless calculations using 'u64' types. 
+        
+        For this purpose, we'll employ a custom math function, 'mul_div_u64', 
+        
+        which performs multiplication first and then division using 'u64' types.
+
+        The fee 'r' computed by this instruction should be an integer between 0 and 500,        
+        
+        corresponding to 0% and 5%, respectively, and not a floating point type.
+
+        Notice that the amount 10000 for 'r' will correspond to 100%.
+
+        */
+
+
+        let fees_amount: u64 = match mul_div_u64(escrow_amount, percentage_fee as u64, (10000 + percentage_fee) as u64)
         {
             Some(fees_amount) => {
                 if escrow_amount < fees_amount {
@@ -77,16 +139,21 @@ pub fn process(
         let to_account = ctx.accounts.validation_authority.to_account_info();
 
         msg!(
-            "Fees to transfer ({} lamports) from escrow to validation authority",
+            "Amount transferred ({} lamports) from escrow to the fees authority due to fees",
             &fees_amount.to_string()
         );
+
         **from_account.try_borrow_mut_lamports()? -= fees_amount;
         **to_account.try_borrow_mut_lamports()? += fees_amount;
 
         msg!(
-            "Remaining lamports in Escrow {}",
+            "Remaining lamports in Escrow {} for the influencer",
             from_account.get_lamports().to_string()
         );
+
+        msg!(
+            "Escrow has changed its state to delivered (2)"
+        )
 
     }
 
