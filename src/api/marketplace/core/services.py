c543ddfe1a841be171d2b4a1dff7fa4b7458d40f
Mudit Mahajan <mudit.m@navtech.io>
2024-04-22 12:18:05 +0530

    Add update_priority_fees function to services.py

--- src/api/marketplace/core/services.py
+++ src/api/marketplace/core/services.py
@@ -2,7 +2,8 @@
 
 import requests
 from decouple import config
-
+from packages.models import Service
+from .models import Configuration
 
 def get_twitter_usage():
     url = "https://api.twitter.com/2/usage/tweets"
@@ -10,3 +11,10 @@ def get_twitter_usage():
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()
+
+
+def update_priority_fees():
+    services = Service.objects.all()
+    current_platform_fees = int(
+        Configuration.objects.get(key="platform_fees").value)
+    services.update(platform_fees=current_platform_fees)
